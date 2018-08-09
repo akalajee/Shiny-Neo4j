d47a077d63dec715410876406344f643c538e9df
Abdulghani Kalaji <akalajee@gmail.com>
2018-08-09 14:46:05 +0400

    - Edit node colors and defaults

--- common.R
+++ common.R
@@ -69,44 +69,31 @@ addVisGroups = function(var_visNetwork, nodes) {
     i = i+1
   }
   
-  groups = list(
-    "Site" = list (groupname = "Site", color =list(
+  var_visNetwork = var_visNetwork %>%  visGroups(useDefaultGroups = FALSE, groupname = "Site", color =list(
       background = "#9EC2F7",
       border = "#3E7EE2",
       highlight = list( background = "#D5E5FD", border = "#3E7EE2" )
-    )),
-    "Site - osn - iib" = list(groupname = "Site - osn - iib", color =list(
+    ))
+  
+  var_visNetwork = var_visNetwork %>%  visGroups(useDefaultGroups = FALSE, groupname = "Site - osn - iib", color =list(
       background = "#EC8584",
       border = "#E74237",
       highlight = list(background = "#F4B2B3", border = "#E74237")
-    )),
-    "Site - iib" = list(groupname = "Site - iib", color =list(
+    ))
+  var_visNetwork = var_visNetwork %>%  visGroups(useDefaultGroups = FALSE, groupname = "Site - iib", color =list(
       background = "#FFFD54",
       border = "#F4AE3D",
       highlight = list( background =  "#FFFEAE", border = "#F4AE3D" )
-    )),
-    "Site - osn" = list(groupname = "Site - osn", color =list(
-      background = "#FFFD54",
-      border = "#F4AE3D",
-      highlight = list(background = "#FFFEAE", border = "#F4AE3D" )
     ))
-  )
-
-  k = 1
-  selected_groups = list()
+  var_visNetwork = var_visNetwork %>%  visGroups(useDefaultGroups = FALSE, groupname = "Site - osn", color =list(
+      background = "#58AB3A",
+      border = "#357533",
+      highlight = list(background = "#9CF08F", border = "#357533" )
+    ))
   
-  while (k<=length(to_be_added)){
-    group_label = to_be_added[k]
-    group_prop = groups[group_label]
-    selected_groups = append(selected_groups, group_prop)
-    k = k + 1
-  }
-  
-  #browser()
-  if(length(selected_groups) > 1)
+  if(length(to_be_added) > 1)
   {
-    print("addVisGroup")
-    var_visNetwork = var_visNetwork %>% visGroups(selected_groups) %>% visLegend()
+    var_visNetwork = var_visNetwork %>% visLegend()
   }
 
   return (var_visNetwork)
