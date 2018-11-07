8f79a14e5c6371b420cbf3d245e3c6174aa90ab5
Abdulghani Kalaji <akalajee@gmail.com>
2018-11-07 09:52:42 +0400

    - Add icon

--- ui.R
+++ ui.R
@@ -3,5 +3,6 @@ require(shiny)
 shiny::shinyUI(shiny::navbarPage(
   title = "Mobile Sites",
   source("./src/ui/sites_ui.R", local = TRUE)$value,
-  source("./src/ui/report_ui.R", local = TRUE)$value
+  source("./src/ui/report_ui.R", local = TRUE)$value,
+  tags$head(tags$link(rel="shortcut icon", href="favicon.ico"))
 ))
